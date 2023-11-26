f2c1d3ced66767a2f1c2709488a274207733528e
YerkoGarces <yerko.garces.c@usach.cl>
2023-11-26 03:15:51 -0300

    Iniciando proyecto, Backend, Frontend, BD, Wireframes

---
+++ Backend/src/main/java/com/Proyecto3/Proyecto3/Proyecto3Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto3.Proyecto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto3Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Proyecto3/Proyecto3/Proyecto3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto3.Proyecto3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
